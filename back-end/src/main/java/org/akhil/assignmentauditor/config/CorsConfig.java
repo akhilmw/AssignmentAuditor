1522455502b103997cbf8a99dc9cb1067bc49de6
Akhil.Nair <akhilmw2@gmail.com>
2024-07-22 17:11:12 +0530

    added more config

--- back-end/src/main/java/org/akhil/assignmentauditor/config/CorsConfig.java
+++ back-end/src/main/java/org/akhil/assignmentauditor/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:5173", "http://assignmentauditor.s3-website.eu-north-1.amazonaws.com", "http://51.20.78.25:5173") // Allow localhost and S3 bucket
+                .allowedOrigins("http://localhost:5173", "http://assignmentauditor.s3-website.eu-north-1.amazonaws.com", "http://51.20.78.25:5173", "http://172.31.27.116:5173", "http://172.27.0.4:5173") // Allow localhost and S3 bucket
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials
